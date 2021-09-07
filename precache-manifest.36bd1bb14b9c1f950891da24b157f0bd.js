7a26f8f729c481d46f7423eab2072d14fdb157d4
ivanjj22 <ivanjj22@users.noreply.github.com>
2021-09-07 19:15:40 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.36bd1bb14b9c1f950891da24b157f0bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca24d51a47073e6f6740d5cda53fe0f3",
+    "url": "https://ivanjj22.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "ccd919317ee3473f80e4",
+    "url": "https://ivanjj22.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "17f4836db810a5c3de0e",
+    "url": "https://ivanjj22.github.io/task-management-frontend/static/js/2.609602ca.chunk.js"
+  },
+  {
+    "revision": "ccd919317ee3473f80e4",
+    "url": "https://ivanjj22.github.io/task-management-frontend/static/js/main.0f742f83.chunk.js"
+  },
+  {
+    "revision": "455258f47c26404f825f",
+    "url": "https://ivanjj22.github.io/task-management-frontend/static/js/runtime~main.713f43b0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://ivanjj22.github.io/task-management-frontend/precache-manifest.36bd1bb14b9c1f950891da24b157f0bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://ivanjj22.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
